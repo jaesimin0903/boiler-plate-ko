c6b5809bc14832d9d439fb92d3d6401b2555b247
jaesimin0903 <78476481+jaesimin0903@users.noreply.github.com>
2022-08-09 02:11:54 +0900

    1

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ mongoose.connect('mongodb+srv://jaesimin:aA5535067!@boiler-plate.tymjh8t.mongodb
     useNewUrlParser : true, useUnifiedTopology:true
 }).then(() => console.log('MongoDB Connected...'))
 .catch(err => console.log(err))
+
 app.get('/',(req,res) => res.send('Hello World'))
 
 app.listen(port, ()=> console.log(`Example app listening on port ${port}!`))
