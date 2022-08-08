91d686f748d38caf295ce37ca7571e23be8ba204
jaesimin0903 <78476481+jaesimin0903@users.noreply.github.com>
2022-08-09 02:10:12 +0900

    Init Repository

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb+srv://jaesimin:aA5535067!@boiler-plate.tymjh8t.mongodb.net/?retryWrites=true&w=majority',{
+    useNewUrlParser : true, useUnifiedTopology:true
+}).then(() => console.log('MongoDB Connected...'))
+.catch(err => console.log(err))
+app.get('/',(req,res) => res.send('Hello World'))
+
+app.listen(port, ()=> console.log(`Example app listening on port ${port}!`))
